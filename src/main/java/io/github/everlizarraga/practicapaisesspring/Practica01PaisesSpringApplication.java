79c5fbf5c2b179dfb6d8a2d5e92fcc3f94b12bcf
Ever Lizarraga <rolank.utn@gmail.com>
2026-06-19 19:28:21 -0300

    etapa 1 completada

--- src/main/java/io/github/everlizarraga/practicapaisesspring/Practica01PaisesSpringApplication.java
+++ src/main/java/io/github/everlizarraga/practicapaisesspring/Practica01PaisesSpringApplication.java
@@ -3,11 +3,15 @@ package io.github.everlizarraga.practicapaisesspring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // = esta es una app Spring Boot, arrancá el framework
 public class Practica01PaisesSpringApplication {
 
   public static void main(String[] args) {
+
+    // Esta linea dice: "empezá a buscar clases desde acá"
     SpringApplication.run(Practica01PaisesSpringApplication.class, args);
+
+    System.out.println("Hola SpringBoot !!!!");
   }
 
 }
