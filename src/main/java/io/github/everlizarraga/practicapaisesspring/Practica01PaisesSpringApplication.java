30c02c2f94c340e8b42b89b9b808b92d357c91d8
Ever Lizarraga <rolank.utn@gmail.com>
2026-06-19 16:17:53 -0300

    intial-commit

---
+++ src/main/java/io/github/everlizarraga/practicapaisesspring/Practica01PaisesSpringApplication.java
@@ -0,0 +1,13 @@
+package io.github.everlizarraga.practicapaisesspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica01PaisesSpringApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Practica01PaisesSpringApplication.class, args);
+  }
+
+}
---
+++ src/test/java/io/github/everlizarraga/practicapaisesspring/Practica01PaisesSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.everlizarraga.practicapaisesspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica01PaisesSpringApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
